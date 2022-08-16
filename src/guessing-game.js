c3ae3f336915b57d6e3fdc990dac8bbc0badea85
anok70 <anok70@mail.ru>
2022-08-16 11:05:33 +0300

    completed task guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,29 @@
-class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
+    class GuessingGame {
+        constructor(number,minNum,maxNum) {
+            this.number = number; 
+            this.minNum = minNum;
+            this.maxNum = maxNum;
+        }
+    
+        setRange(min, max) {
+            this.minNum = min;
+            this.maxNum = max;
+        }
+    
+        guess() {
+    return Math.ceil((this.maxNum + this.minNum)/2);
+        }
+    
+        lower() {
+            this.maxNum = Math.ceil((this.maxNum + this.minNum)/2);
+        }
+    
+        greater() {
+            this.minNum = Math.ceil((this.maxNum + this.minNum)/2);
+    
+        }
     }
+    
+    module.exports = GuessingGame;
 
-    guess() {
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
-}
-
-module.exports = GuessingGame;
